472e65799eb509b1d4881bf3c0b6bf57701cb5aa
Adnan-Sait <59290226+Adnan-Sait@users.noreply.github.com>
2024-02-24 22:59:44 +0530

    Error handling (#10)
    * Weather: Added error labels
    
    * Weather: Error handling added for JSON file load
    
    * Fixed label store bug
    
    * Fixed main function
    
    * Error handling added for service calls

--- weather-chat-cli/index.js
+++ weather-chat-cli/index.js
@@ -5,19 +5,33 @@ import store from './store/store.js';
 /**
  * @type {import('./types/index.js').Prompt[]}
  */
-const promptsJson = JSON.parse(fs.readFileSync('./data/prompts.json'));
+let promptsJson;
 /**
  * @type {import('./types/index.js').User[]}
  */
-const usersJson = JSON.parse(fs.readFileSync('./data/users.json'));
-const labelsJson = JSON.parse(fs.readFileSync('./data/labels.json'));
-
-store.dispatch({ type: 'label/setLabel', payload: labelsJson });
+let usersJson;
+/**
+ * @type {Object}
+ */
+let labelsJson;
 
 /**
  * Main Function.
  */
-async function main() {
+function main() {
+  try {
+    promptsJson = JSON.parse(fs.readFileSync('./data/prompts.json'));
+    usersJson = JSON.parse(fs.readFileSync('./data/users.json'));
+    labelsJson = JSON.parse(fs.readFileSync('./data/labels.json'));
+
+    store.dispatch({ type: 'label/setLabel', payload: labelsJson });
+  } catch (err) {
+    throw new Error(
+      `Error loading config data. Please report this issue to the administrator.`,
+      { cause: err },
+    );
+  }
+
   startChat(promptsJson, usersJson);
 }
 
