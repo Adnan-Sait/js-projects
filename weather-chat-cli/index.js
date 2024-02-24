a177acc2f11b9777d5670c29d287c20f13761fe8
Adnan Sait <adnansait74@gmail.com>
2024-02-24 22:38:09 +0530

    Fixed main function

--- weather-chat-cli/index.js
+++ weather-chat-cli/index.js
@@ -18,21 +18,21 @@ let labelsJson;
 /**
  * Main Function.
  */
-async function main() {
+function main() {
+  try {
+    promptsJson = JSON.parse(fs.readFileSync('./data/prompts.json'));
+    usersJson = JSON.parse(fs.readFileSync('./data/users.json'));
+    labelsJson = JSON.parse(fs.readFileSync('./data/labels.json'));
+
+    store.dispatch({ type: 'label/setLabel', payload: labelsJson });
+  } catch (err) {
+    throw new Error(
+      `Error loading config data. Please report this issue to the administrator.`,
+      { cause: err },
+    );
+  }
+
   startChat(promptsJson, usersJson);
-}
-
-try {
-  promptsJson = JSON.parse(fs.readFileSync('./data/prompts.json'));
-  usersJson = JSON.parse(fs.readFileSync('./data/users.json'));
-  labelsJson = JSON.parse(fs.readFileSync('./data/labels.json'));
-
-  store.dispatch({ type: 'label/setLabel', payload: labelsJson });
-} catch (err) {
-  throw new Error(
-    `Error loading config data. Please report this issue to the administrator.`,
-    { cause: err },
-  );
 }
 
 main();
