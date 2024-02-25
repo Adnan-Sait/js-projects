13a7e0dd58e9dbd801e642e2fa6686b8c9767097
Adnan Sait <adnansait74@gmail.com>
2024-02-25 21:27:40 +0530

    Implemented promise synchronization for reading files

--- weather-chat-cli/index.js
+++ weather-chat-cli/index.js
@@ -1,38 +1,39 @@
-import fs from 'node:fs';
+import { promises as fsPromises } from 'fs';
 import { startChat } from './operations/chat.op.js';
 import store from './store/store.js';
 
 /**
- * @type {import('./types/index.js').Prompt[]}
- */
-let promptsJson;
-/**
- * @type {import('./types/index.js').User[]}
- */
-let usersJson;
-/**
- * @type {Object}
- */
-let labelsJson;
-
-/**
  * Main Function.
  */
-function main() {
+async function main() {
   try {
-    promptsJson = JSON.parse(fs.readFileSync('./data/prompts.json'));
-    usersJson = JSON.parse(fs.readFileSync('./data/users.json'));
-    labelsJson = JSON.parse(fs.readFileSync('./data/labels.json'));
+    const [promptBuffer, userBuffer, labelBuffer] = await Promise.all([
+      fsPromises.readFile('./data/prompts.json'),
+      fsPromises.readFile('./data/users.json'),
+      fsPromises.readFile('./data/labels.json'),
+    ]);
+
+    /**
+     * @type {import('./types/index.js').Prompt[]}
+     */
+    const promptsJson = JSON.parse(promptBuffer);
+    /**
+     * @type {import('./types/index.js').User[]}
+     */
+    const usersJson = JSON.parse(userBuffer);
+    /**
+     * @type {Object}
+     */
+    const labelsJson = JSON.parse(labelBuffer);
 
     store.dispatch({ type: 'label/setLabel', payload: labelsJson });
+    startChat(promptsJson, usersJson);
   } catch (err) {
     throw new Error(
       `Error loading config data. Please report this issue to the administrator.`,
       { cause: err },
     );
   }
-
-  startChat(promptsJson, usersJson);
 }
 
 main();
